db6b6bbf68784546cca17260b7a558d7053bbc3f
JCameron17 <54552570+JCameron17@users.noreply.github.com>
2020-03-31 15:13:39 -0400

    Update recursion.s
    additional characters

--- recursion.s
+++ recursion.s
@@ -27,4 +27,9 @@ main:
       addi $v1, $0, 48          #value to be subtracted in multVal
       blt $a0, 58, Accepted     #accept digits
       blt $a0, 65, notAccepted  #special characters not accepted
+      addi $v1, $0, 55          #value to be subtracted in multVal
+    	blt $a0, 87, Accepted     #accept chars a-v
+      blt $a0, 97, notAccepted  #do not accept special characters
+      addi $v1, $0, 87          #value to be subtracted in multVal
+      blt $a0, 119, Accepted     #accept characters a-v
 
