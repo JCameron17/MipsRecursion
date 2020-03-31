c5abc449baca8cbea3dbf5f5c0c98e8560075be7
JCameron17 <54552570+JCameron17@users.noreply.github.com>
2020-03-31 15:02:42 -0400

    Update recursion.s
    begin defining which characters will be accepted

--- recursion.s
+++ recursion.s
@@ -21,4 +21,10 @@ main:
      invalidMessage:
       li $v0, 0
       la $t0, invalid   #load message to print for invalid input
+      
+       decideLoop:
+      blt $a0, 48, notAccepted  #character with ascii value < 48 is not accepted
+      addi $v1, $0, 48          #value to be subtracted in multVal
+      blt $a0, 58, Accepted     #accept digits
+      blt $a0, 65, notAccepted  #special characters not accepted
 
